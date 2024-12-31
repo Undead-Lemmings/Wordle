bb1d451b3bc0ea1a9dbe055e1432307f40ed90a4
Ben <ben@munrotech.co.uk>
2024-12-31 19:43:46 +0000

    Improvements to POC. Now able to import words

--- POC.py
+++ POC.py
@@ -1,8 +1,21 @@
 # Demo functionality
+import random
 
-word = "LOLES"
+# Get the words as an array
 
-guess = "LINLD"
+wordlist = []
+
+
+
+with open("wordlist.txt", "r") as file:
+    raw_words = file.readlines()
+    for possible_word in raw_words:
+        wordlist.append(possible_word.replace("\n", "",))
+
+print(wordlist)
+
+word = random.choice(wordlist).upper()
+print(word)
 
 # FFS I am a moron. I can just get the index of word using guess. Input verification can happen before doing this
 """ 
@@ -14,6 +27,7 @@ for word_letters in range(len(word)):
         if guess_letter == word_letter:
             print("Is in the correct place!")
 """
+guess = input("Please enter a five letter word: ").upper()
 
 for letter_index in range(len(guess)):
     if guess[letter_index] == word[letter_index]:
