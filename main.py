d232746622b5749084230bbed2b0d81c02899c5a
Undead-Lemmings <ben@munrotech.co.uk>
2025-01-05 16:58:18 +0000

    Demo GitHub (#2)
    * Created a Proof Of Concept with hardcoded words
    
    * new file:   POC.py
    
    * Improvements to POC. Now able to import words
    
    * Tired, need a break. Making tea
    
    * Address possible change -
    Iterate over correct letter/correct place as
    first step
    
    * General updates to functionality
    
    * General improvements
    
    * Initial Playable Version - Letters not working
    
    * Update main.py
    
    * Initial Branch Demo
    
    * Functionality added - COLOUR! also returns letters
    and does minor amount of input validation

--- main.py
+++ main.py
@@ -4,6 +4,28 @@
 
 from random import choice
 
+# my Dot made colours in her Wordle and by gosh do I want to do the same now >:[
+def colourise(funguess):
+    colourguess = []
+    # ANSI Escape sequences seem to be the easiest (not involving additional libraries) way do this
+    # colours needed 32 - green     = "\033[32mCorrectLetterCorrectPlace\033[0m"
+    #                33 - Yellow    =  "\033[33mCorrectLetterWrongPlace\033[0m"
+    # function strips brackets, adds colour, moves on with life idk
+    for i in funguess:
+        if "[" in i:
+            i = i.replace("[", "")
+            i = i.replace("]","")
+            i = f"\033[32m{i}\033[0m"
+            colourguess.append(i)   
+        elif "(" in i:
+            i = i.replace("(", "")
+            i = i.replace(")", "")
+            i = f"\033[33m{i}\033[0m"
+            colourguess.append(i)
+        else:
+            colourguess.append(i)
+    return " ".join(colourguess)
+  
 # Chooses the answer word
 def select_word():
     wordlist = []
@@ -11,57 +33,76 @@ def select_word():
         raw_words = file.readlines()
         for possible_word in raw_words:
             wordlist.append(possible_word.replace("\n", "",))
-    return choice(wordlist).upper()
+    return wordlist
 
 # Tracks number of guesses
 def guess_no(total_guesses, number):
     return total_guesses - number
 
-# Provides last guess results
+def input_validation(guess):
+    words = select_word()
+    if guess.lower() in words:
+        return True
+    else:
+        return False
+
+# Checks for [Correct Letter/Correct Place] and (Correct Letter/Wrong Place) 
 def guess_results(guess,word):
     status = ["_"] * len(word)
-    # for loop  - makes sure correct letter/correct place is returned with []
     for i in range(len(guess)):
         if guess[i] == word[i]:
             status[i] = f"[{guess[i]}]"
-    # for loop  - checks for correct letter/wrong plcae () and that the number of occurences in guess is less than the occurences in status
     for i in range(len(guess)):
         if status[i] != f"[{guess[i]}]" and guess[i] in word and word.count(guess[i]) > (status.count(f"[{guess[i]}]") + status.count(f"({guess[i]})")):
                 status[i] = f"({guess[i]})"
-    # makes sure the _ replaces any wrong letters
-    for i in range(len(guess)):
-        if guess[i] not in word:
-            status[i] = "_"
-    return " ".join(status)
+    return status
 
 # shows available letters
-def alphabet(guess, word):
-    pass
-
+def alphabet(letters, guess, word):
+    new_remaining_letters = []
+    for i in letters:
+        if i in guess:
+            if i in word:
+                new_remaining_letters.append(f"\033[32m{i}\033[0m")
+            else:
+                new_remaining_letters.append("_")
+        else:
+            new_remaining_letters.append(i)
+    return new_remaining_letters
+    
 def main():
     gaming = True
     guess = ""
+    remaining_letters = ["A","B","C","D","E","F","G","H","U","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     tguesses = 6
     nguesses = 0
-    # remaining_letters = ["A","B","C","D","E","F","G","H","U","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    word = select_word()
-    # print(word) #used for testing
+    word = choice(select_word()).upper()
+    print(word)
     while gaming:
         guess = input("Please enter your word: ").upper()
-        nguesses += 1
-        print(guess_results(guess, word))
-        print(f"You have {guess_no(tguesses,nguesses)} guesses left!")
-        if guess_no(tguesses, nguesses) == 0:
-            print("*********************************************************")
-            print(f"AW NO! YOU HAVE LOST THE WORDLE! THE CORRECT WORD WAS {word.upper()}")
-            print("*********************************************************")
-            gaming = False
-        # Nat - use guest == word instead of "".join(guess_results(guess, word)).replace("[","").replace("]","") == word
-        if guess == word:
-            print("*********************************************************")
-            print("CONGRATULATIONS! YOU WIN WORDLE! YOU LITTLE WORDLER 😘😘")
-            print("*********************************************************")
-            gaming = False
+        if input_validation(guess):    
+            nguesses += 1
+            # Uses guess function to get letters as a list, transforms them into colours and returns string with colourise fun
+            print(colourise(guess_results(guess, word)))
+            # Update and print the remaining letters
+            alphabet(remaining_letters, guess, word)
+            remaining_letters = alphabet(remaining_letters, guess, word)
+            print(" ".join(remaining_letters))
+            # Response to guess correct/out of turns/next round
+            if guess == word:
+                print("*********************************************************")
+                print("CONGRATULATIONS! YOU WIN WORDLE! YOU LITTLE WORDLER 😘😘")
+                print("*********************************************************")
+                gaming = False
+            elif guess_no(tguesses, nguesses) == 0:
+                print("*********************************************************")
+                print(f"AW NO! YOU HAVE LOST THE WORDLE! THE CORRECT WORD WAS {word.upper()}")
+                print("*********************************************************")
+                gaming = False
+            else:
+                print(f"You have {guess_no(tguesses,nguesses)} guesses left!")
+        else:
+            print("SO SORRY! THAT WORD IS NOT ON OUR LIST BUT WE ARE CERTAIN IT WAS AN EXCELLENT WORD!")
 
 if __name__ == "__main__":
     main()
